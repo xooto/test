e56b5d77b87818da8e91db080a8447dd40e97b03
xooto <51514025+xooto@users.noreply.github.com>
2020-05-02 14:37:19 +0300

    Update settings.py

--- loadVideo/settings.py
+++ loadVideo/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'loadVideo.urls'
@@ -125,5 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
 os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 gjango_heroku.settings(locals())
