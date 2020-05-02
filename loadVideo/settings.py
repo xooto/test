b9e8ff7cdbf2987f429bbd80f11ec776dcb3924f
xooto <51514025+xooto@users.noreply.github.com>
2020-05-02 14:24:52 +0300

    Update settings.py

--- loadVideo/settings.py
+++ loadVideo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -120,3 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+gjango_heroku.settings(locals())
