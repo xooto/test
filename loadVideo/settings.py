e8ec728b53fbc2029c9338b56b68429219a5ad60
xooto <51514025+xooto@users.noreply.github.com>
2020-05-02 14:43:40 +0300

    Update settings.py

--- loadVideo/settings.py
+++ loadVideo/settings.py
@@ -123,10 +123,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-os.path.join(BASE_DIR, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 gjango_heroku.settings(locals())
